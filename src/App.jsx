58d0358d37e14b529036f7262c09973cc6c8dc2b
abdumalik-cc <rakhmatovabdumalik5@gmail.com>
2025-06-10 17:37:54 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,217 @@
+import React from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+<div>
+
+<header>
+<div className="container">
+    <div className="h-ota">
+    <img src="./Frame 1.png" alt=""/>
+    <nav>
+        <ul>
+            <li><a href="">Demos</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Pages</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+    </div>
+    <button className='bt-green'>Get Started</button>
+</div>
+</header>
+
+<main>
+
+<section className='s1'>
+     <div className="container">
+          <div className="s1ota">
+                <div className="s1t">
+                    <h1>Organized cloud <br />and web hosting <br />for your business</h1>
+                    <p>Unlimited Domain & Hosting in One Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, easy WordPress installs.</p>
+                    <button className='bt-green'>Get started</button>
+                    <button className='bt-w'>View packages</button>
+                </div>
+                <img src="./elements 1.png" alt="" />
+          </div>
+     </div>
+</section>
+
+<section className='s2'>
+    <div className="container">
+            <h1>These features make <br />your hosting work faster</h1>
+            <p>Unlimited Domain & Hosting in One Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, easy WordPress installs.</p>
+         <div className="s2ota">
+               <div className="s2card">
+                   <img src="./icon.png" alt="" />
+                   <h2>Scale to success</h2>
+                   <p>Unlimited Domain & Hosting in One <br />Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, <br />easy WordPress installs.</p>
+               </div>
+               <div className="s2card">
+                   <img src="./icon.png" alt="" />
+                   <h2>Scale to success</h2>
+                   <p>Unlimited Domain & Hosting in One <br />Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, <br />easy WordPress installs.</p>
+               </div>
+               <div className="s2card">
+                   <img src="./icon.png" alt="" />
+                   <h2>Scale to success</h2>
+                   <p>Unlimited Domain & Hosting in One <br />Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, <br />easy WordPress installs.</p>
+               </div>
+         </div>
+    </div>
+</section>
+
+<section className='s3'>
+    <div className="container">
+         <div className="s3ota">
+              <h1>Your websites deserve <br />fast , secure and also <br />reliable hosting</h1>
+              <p>Unlimited Domain & Hosting in One Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, easy WordPress installs.Unlimited <br />Domain & Hosting in One Platform.Website hosting options, 99.9% uptime <br />guarantee, free SSL certificate, easy WordPress installs.</p>
+         </div>
+         <img src="./dashboard 1.png" alt="" />
+    </div>
+</section>
+
+<section className='s1'>
+     <div className="container">
+          <div className="s1ota">
+                <img src="./elements 1.png" alt="" />
+                <div className="s1t">
+                    <h1>Lets get rid of worries <br />about web hosting to <br />save your time</h1>
+                    <p>Unlimited Domain & Hosting in One Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, easy WordPress installs.</p>
+                    <button className='bt-green'>Explore now</button>
+                </div>
+          </div>
+          <h1 className='h1'>Innovative hosting platform <br />
+          built team empowerment</h1>
+          <p className='p'>Unlimited Domain & Hosting in One Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, easy WordPress installs.</p>
+
+          <ul>
+             <li><a href="">Block Storage</a></li>
+             <li><a href="">Load balancers</a></li>
+             <li><a href="">Floating IPS</a></li>
+          </ul>
+
+          <div className="s1ota">
+                <div className="s1t">
+                    <h1>Block storage</h1>
+                    <p>Unlimited Domain & Hosting in One Platform.Website hosting options, 99.9% <br />uptime guarantee, free SSL certificate, easy WordPress installs.</p>
+                    <button className='bt-green'>Explore now</button>
+                </div>
+                <img src="./elements3 1.png" alt="" />
+          </div>
+     </div>
+</section>
+
+<section className='s2'>
+    <div className="container">
+            <h1>Choose your perfect <br />web hosting plan</h1>
+            <p>Each web hosting plan comes with a free domain name</p>
+            <div className="btns">
+            <button className='bt-green'>Monthly</button>
+            <button className='bt-w'>Yeraly</button>
+            </div>
+         <div className="s2ot">
+               <div className="s2card">
+                   <img src="./chaqmoq.png" alt="" />
+                   <h2>Agency</h2>
+                   <h3>Best for medium agency</h3>
+                   <h1>190$</h1>
+                   <ul>
+                     <li><p>16x cpu power</p></li>
+                     <li><p>Free migration</p></li>
+                     <li><p>Limited Websites</p></li>
+                     <li><p>100 GB all ssd storage</p></li>
+                     <li><p>Shared environment</p></li>
+                     <li><p>Unlimited bandwidth</p></li>
+                   </ul>
+                   <button className='bt-green'>Start 14 days trial</button>
+                   <h3>No credit card required</h3>
+               </div>
+               <div className="s2card">
+                   <img src="./chaqmoq.png" alt="" />
+                   <h2>Agency</h2>
+                   <h3>Best for medium agency</h3>
+                   <h1>190$</h1>
+                   <ul>
+                     <li><p>16x cpu power</p></li>
+                     <li><p>Free migration</p></li>
+                     <li><p>Limited Websites</p></li>
+                     <li><p>100 GB all ssd storage</p></li>
+                     <li><p>Shared environment</p></li>
+                     <li><p>Unlimited bandwidth</p></li>
+                   </ul>
+                   <button className='bt-green'>Start 14 days trial</button>
+                   <h3>No credit card required</h3>
+               </div>
+               <div className="s2card">
+                   <img src="./chaqmoq.png" alt="" />
+                   <h2>Agency</h2>
+                   <h3>Best for medium agency</h3>
+                   <h1>190$</h1>
+                   <ul>
+                     <li><p>16x cpu power</p></li>
+                     <li><p>Free migration</p></li>
+                     <li><p>Limited Websites</p></li>
+                     <li><p>100 GB all ssd storage</p></li>
+                     <li><p>Shared environment</p></li>
+                     <li><p>Unlimited bandwidth</p></li>
+                   </ul>
+                   <button className='bt-green'>Start 14 days trial</button>
+                   <h3>No credit card required</h3>
+               </div>
+         </div>
+    </div>
+</section>
+
+<section className='s4'>
+    <div className="container">
+         <h1>See what these <br />Customers have to say</h1>
+         <div className="s4ota">
+               <div className="s4box">
+                  <img src="./Frame 48.png" alt="" />
+                  <p>“I cannot believe that I have got a brand <br />new landing page after getting Omega. It <br />was super easy to edit and <br />faster and easier to work & increases our exposure.”</p>
+                  <h3>Jenny wilson</h3>
+                  <h4>Chief marketing officer</h4>
+               </div>
+               <div className="s4box">
+                  <img src="./Frame 48.png" alt="" />
+                  <p>“I cannot believe that I have got a brand <br />new landing page after getting Omega. It <br />was super easy to edit and <br />faster and easier to work & increases our exposure.”</p>
+                  <h3>Jenny wilson</h3>
+                  <h4>Chief marketing officer</h4>
+               </div>
+         </div>
+
+         <div className="end">
+              <h1>Stop worrying about <br />
+              hosting problems</h1>
+              <button className='bt-w'>SIgn up now</button>
+         </div>
+    </div>
+</section>
+
+
+
+</main>
+
+
+<footer>
+    <div className="container">
+         <img src="./Frame 1.png" alt="" />
+         <ul>
+            <li><a href="">Demos</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Pages</a></li>
+            <li><a href="">Contact</a></li>
+         </ul>
+    </div>
+</footer>
+
+</div>
+  )
+}
+
+export default App
